93444c9e8f00dce4e7b8f127b81c826d648e89f4
Andrew Thyng <andrew.thyng@gmail.com>
2017-07-31 20:03:03 -0500

    Begin writing unit tests

---
+++ tests/ClientGameManagerTests.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClientGameManagerTests {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void givenConditionThenResult() {
+
+    }
+}
