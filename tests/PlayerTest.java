598e863af99504aeb49e65c76be8117680ba4744
StanleyThepkaisone <stanleythepkaisone@gmail.com>
2017-08-08 18:37:29 -0700

    Add files via upload
    Player Test

---
+++ tests/PlayerTest.java
@@ -0,0 +1,46 @@
+package edu.asu.stratego.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+class PlayerTest {
+
+    Player testPlayerTest = new Player();
+    String nickname = "TestNickName";
+    String result;
+    PieceColor color = PieceColor.BLUE;
+    PieceColor testColor;
+
+
+    @Test
+    void setNickname() {
+        testPlayerTest.setNickname(nickname);
+        result = testPlayerTest.getNickname();
+        assertThat(result.toString(), is("TestNickName"));
+    }
+
+    @Test
+    void getNickname() {
+        testPlayerTest.setNickname(nickname);
+        result = testPlayerTest.getNickname();
+        assertThat(result.toString(), is("TestNickName"));
+    }
+
+    @Test
+    void setColor() {
+        testPlayerTest.setColor(color);
+        testColor = testPlayerTest.getColor();
+        assertThat(testColor.toString(), is("BLUE"));
+
+    }
+
+    @Test
+    void getColor() {
+        testPlayerTest.setColor(color);
+        testColor = testPlayerTest.getColor();
+        assertThat(testColor.toString(), is("BLUE"));
+
+    }
+
+}
